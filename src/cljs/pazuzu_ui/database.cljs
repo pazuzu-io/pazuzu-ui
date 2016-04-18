581e8800ed3c1a09dbedeb8b3c5b3ceb8342b5c1
Anton Chebotaev <anton.chebotaev@zalando.de>
2016-04-18 17:45:40 +0200

    #2 initial project structure

---
+++ src/cljs/pazuzu_ui/database.cljs
@@ -0,0 +1,5 @@
+(ns pazuzu-ui.database)
+
+(def default-db
+  {:name "pazuzu-ui"
+   :active-page :home-page})
---
+++ src/cljs/pazuzu_ui/routes.cljs
@@ -0,0 +1,26 @@
+(ns pazuzu-ui.routes
+    (:require [taoensso.timbre :as log :include-macros true]
+              [re-frame.core :refer [dispatch]]
+              [bidi.bidi :as bidi]
+              [pushy.core :as pushy]))
+
+(log/debug "Registering routes")
+
+(def routes ["/" {""         :home-page
+                  "about"    :about-page
+                  "registry" :registry-page
+                  true       :not-found}])
+
+(defn- parse-url [url]
+  (bidi/match-route routes url))
+
+(defn- dispatch-route [matched-route]
+  (let [{handler :handler} matched-route]
+    (log/debug "Mathced route -" handler)
+    (dispatch [:set-active-page handler])))
+
+(defn init-routes []
+  (log/debug "Initializing routes")
+  (pushy/start! (pushy/pushy dispatch-route parse-url)))
+
+(def url-for (partial bidi/path-for routes))
