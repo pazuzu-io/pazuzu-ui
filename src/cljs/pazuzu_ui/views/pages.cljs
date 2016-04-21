fda87d523c33db90ece9e863de261d1a6d6f3851
Anton Chebotaev <anton.chebotaev@zalando.de>
2016-04-21 12:57:02 +0200

    #3 completed feature pane

--- src/cljs/pazuzu_ui/views/pages.cljs
+++ src/cljs/pazuzu_ui/views/pages.cljs
@@ -1,5 +1,6 @@
 (ns pazuzu-ui.views.pages
-  (:require [re-frame.core :refer [subscribe dispatch]]))
+  (:require [re-frame.core :refer [subscribe dispatch]]
+            [taoensso.timbre :as log]))
 
 (defn welcome []
   [:div.ui.vertical.masthead.center.aligned.segment
@@ -17,17 +18,44 @@
    [:h2 "About Pazuzu"]
    [:p "An evil ancient moster"]])
 
-(defn feature-list-item [name]
+(defn feature-list-item [props]
   [:div.ui.card.feature
    [:div.content
-    [:img.right.floated.ui.image {:src "https://placekitten.com/g/50/50"}]
-    [:div.header name]
+    #_[:img.right.floated.ui.image {:src "https://placekitten.com/g/50/50"}]
+    [:div.header (:name props)]
     [:div.meta "Dependencies:" [:a {:href "#"} "python"]]
     [:div.description "Feature description"]]])
 
 (defn feature-details [name]
-  [:div.ui.huge.input
-   [:input {:type "text" :placeholder "Feature Name" :value name}]])
+  [:div.ui.grid.container
+   [:div.row
+    [:div.column
+     [:div.ui.form
+      [:div.field
+       [:div.ui.huge.input.fluid
+        [:input {:type "text" :placeholder "Feature Name"
+                 :value name
+                 :on-change (fn [e] (log/debug e))}]]]
+      [:div.field
+       [:label "Dependencies"]
+       [:div.ui.label "python"]
+       [:div.ui.label "scala"]]
+      [:div.field.code
+       [:label "Snippet"]
+       [:textarea {:field :textarea
+                   :rows 5
+                   :value "RUN apt-get install leiningen"
+                   :on-change (fn [e] (log/debug e))}]]
+      [:div.field.code
+       [:label "Test Case command"]
+       [:textarea {:field :textarea
+                   :rows 3
+                   :value "lein -v"
+                   :on-change (fn [e] (log/debug e))}]]
+      [:div.field
+       [:label "Attached files"]
+       [:div.ui.label "id-rsa.pub"]
+       [:button.mini.ui.basic.button [:i.icon.upload] "Upload file"]]]]]])
 
 (defn registry []
   [:div#registry.ui.padded.grid
@@ -41,7 +69,7 @@
       [:div.item [:div.ui.primary.button "New"]]]]
 
     [:div.features-list.ui.cards
-     (for [i (range 100)] [feature-list-item (str "feature " i)])]]
+     (for [i (range 100)] [feature-list-item {:key i :name (str "feature " i)}])]]
 
    [:div#feature-details.eight.wide.column
     [:div.ui.secondary.menu
