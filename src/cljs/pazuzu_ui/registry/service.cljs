e70e0663db03f2c7648b13109ce0b81d7ce0f983
Szymon Bochniak <bochniak.szymon@gmail.com>
2016-05-20 12:10:08 +0200

    #6 Make POST/PUT feature work by flattening the dependencies

--- src/cljs/pazuzu_ui/registry/service.cljs
+++ src/cljs/pazuzu_ui/registry/service.cljs
@@ -2,7 +2,14 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [pazuzu-ui.config :as conf]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]]
+            [taoensso.timbre :as log]))
+
+(defn flatten_dependencies
+  "Takes featrue and changes the dependency objects vector to plane names vector"
+  [feature]
+  (let [dependencies_names (vec (map #(:name %) (:dependencies feature)))]
+    (assoc-in feature [:dependencies] dependencies_names)))
 
 (defn get-features
   "Get the features and execute the callback with the features as argument"
@@ -20,11 +27,15 @@
 (defn add-feature
   "Add a new feature to the registry"
   [feature callback]
-  (go (let [response (<! (http/post (str conf/registry-api "/api/features") {:json-params feature}))]
+  (go (let [response (<! (http/post
+                           (str conf/registry-api "/api/features") {:json-params (flatten_dependencies feature)}))]
         (callback (:body response)))))
 
 (defn update-feature
   "Update a feature in the registry"
   [feature callback]
-  (go (let [response (<! (http/put  (str conf/registry-api "/api/features/" (:name feature)) {:json-params feature}))]
+  (go (let [response (<! (http/put
+                           (str conf/registry-api "/api/features/" (:name feature))
+                           {:json-params (flatten_dependencies feature)}))]
         (callback (:body response)))))
+
