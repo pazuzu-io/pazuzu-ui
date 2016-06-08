a6d99fc879eda2cb6c69debee998463b475cea35
vpavlyshyn <gfdkbiby>
2016-06-08 17:15:06 +0200

    #34: restore default endpoint

--- src/cljs/pazuzu_ui/config.cljs
+++ src/cljs/pazuzu_ui/config.cljs
@@ -8,6 +8,6 @@
 
 ;;load environment variable or get the default one, will be loaded at compile time
 (env/def
-  BACKEND_ENDPOINT "http://192.168.99.100:8080")
+  BACKEND_ENDPOINT "http://localhost:8080")
 
 (def registry-api BACKEND_ENDPOINT)
