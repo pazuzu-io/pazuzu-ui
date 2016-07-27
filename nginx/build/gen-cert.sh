704940b7e4d03db1cf52e01bedb6f850a54fbb56
Daniel Truemper <daniel.truemper@zalando.de>
2016-07-27 16:38:20 +0200

    Removes unsused certificates

--- nginx/build/gen-cert.sh
+++ nginx/build/gen-cert.sh
@@ -16,13 +16,7 @@ HOSTNAME=$1
 
 echo Generating SSL certificate for "$HOSTNAME"
 
-#openssl genrsa -des3 -passout pass:x -out ${FILENAME}.pass.key 2048
-#openssl rsa -passin pass:x -in ${FILENAME}.pass.key -out ${FILENAME}.key
-#openssl req -new -key ${FILENAME}.key -out ${FILENAME}.csr -subj "/CN=${CN}"
-#openssl x509 -req -days 365 -in ${FILENAME}.csr -signkey ${FILENAME}.key -out ${FILENAME}.crt
-
 openssl genrsa -out cert.key 4096
 # Use piping to avoid *.csr file creation
 openssl req -new -key cert.key -subj "/CN=$HOSTNAME" \
     | openssl x509 -req -sha256 -days 36526 -CA ca.crt -CAkey ca.key -set_serial 01 -out cert.crt -extensions v3_req
-
