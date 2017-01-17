0c03615fa3fcde67b09031c78b212bcb9a81b1a4
Marco Lehmann <marco.lehmann@zalando.de>
2017-01-17 09:50:40 +0100

    rearranged feature detail code, added comments, added feature existing check

--- src/app/feature-detail/feature-detail.component.ts
+++ src/app/feature-detail/feature-detail.component.ts
@@ -20,37 +20,81 @@ export class FeatureDetailComponent implements OnInit, OnDestroy {
   heading: string;
   feature: Feature;
 
+  /**
+   * update internal state
+   * @returns {void} nothing
+   */
   update() {
+
+    // get name parameter
     this.name = this.route.snapshot.params['name'];
+
+    // get feature by name
     this.feature = this.featureService.getFeature(this.name);
+
+    // if feature was not found redirect to list
+    if (!this.feature) {
+      this.router.navigate(['/features/list']);
+    }
+
+    // update heading and title
     this.heading = `Feature details for ${this.name}`;
     this.eventBusService.emit(APP_TITLE_CHANGE, this.heading);
+
   }
 
+  /**
+   * go back using Location API
+   * @returns {void} nothing
+   */
   goBack() {
+
+    // go back
     this.location.back();
+
   }
 
+  /**
+   * constructor
+   * @param {Router} router
+   * @param {ActivatedRoute} route
+   * @param {Location} location
+   * @param {EventBusService} eventBusService
+   * @param {FeatureService} featureService
+   * @returns {void} nothing
+   */
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private location: Location,
     private eventBusService: EventBusService,
     private featureService: FeatureService
-  ) {
+  ) { }
 
-    this.sub = router.events.subscribe((val) => {
+  /**
+   * on init handler
+   * subscribe to parameter changes
+   * @returns {void} nothing
+   */
+  ngOnInit() {
+
+    // subscribe to param changes if somebody manually changes them
+    this.sub = this.router.events.subscribe((val) => {
+
+      // if event is of type NavigationEnd update internal state
       if (val instanceof NavigationEnd) {
         this.update();
       }
+
     });
 
   }
 
-  ngOnInit() {
-    this.update();
-  }
-
+  /**
+   * on destroy handler
+   * unsubscribe from parameter changes
+   * @returns {void} nothing
+   */
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
